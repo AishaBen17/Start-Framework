e98290065ce2c361010d76339351bf656c6aa13a
AishaBen17 <aisha.ahmedben@gmail.com>
2025-01-13 15:11:49 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
-import './index.css';
+
 
 
 createRoot(document.getElementById('root')).render(
