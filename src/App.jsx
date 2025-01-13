25d23ecce9add1227b59aee0c31abd89f2eedba9
AishaBen17 <aisha.ahmedben@gmail.com>
2025-01-13 15:11:49 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import Layout from '../Components/Layout/Layout'
-import { createBrowserRouter } from 'react-router-dom'
 import Home from '../Components/Home/Home'
 import Portfolio from './../Components/Portfolio/Portfolio';
 import Contact from './../Components/Contact/Contact';
 import About from '../Components/About/About'
 import Error from './../Components/Error/Error';
+// import { createBrowserRouter } from 'react-router-dom' 
+import { createHashRouter } from 'react-router-dom';
 
 export default function App() {
-    let x = createBrowserRouter([
-      
-        { path:"/", 
-            element: <Layout /> ,
-             children:[
-            { path:"", element:<Home/> },
-            { path:"about", element:<About/>},
-            { path:"portfolio", element:<Portfolio/>},
-            { path:"contact", element:<Contact/>},
-            { path:"*", element:<Error />},
-        ]
-         },
-       
-    ])
+
+
+    let x = createHashRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                { index: true, element: <Home /> },
+                { path: "about", element: <About /> },
+                { path: "portfolio", element: <Portfolio /> },
+                { path: "contact", element: <Contact /> },
+                { path: "*", element: <Error /> },
+            ],
+        },
+    ]);
+    
 
     return <>
         <RouterProvider router={x} />
